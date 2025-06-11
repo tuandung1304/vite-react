f90dc9f4797e4a07244a2024e85f538d92db3b82
tuandung1304 <tuandung13401@gmail.com>
2025-06-11 21:44:18 +0700

    Install and setup shadcn

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="h-[100vh] w-[100vw] bg-slate-700">
+      <h1 className="text-center text-white">Home</h1>
+    </div>
+  )
+}
